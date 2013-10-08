e51dbd1d6946b263051d02cf97c89e645cdcad1d
jainshi <jainshi@gmail.com>
2013-10-08 21:28:16 +0530

    spring

---
+++ learn-spring-maven/src/main/java/test/spring/aspect/calculator/CalculatorLoggingAspect.java
@@ -0,0 +1,16 @@
+package test.spring.aspect.calculator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class CalculatorLoggingAspect {
+	private Log log = LogFactory.getLog(this.getClass());
+
+	@Before("execution(* ArithmeticCalculator.add(..))")
+	public void logBefore() {
+		log.info("The method add() begins");
+	}
+}
