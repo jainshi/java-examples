0ebde50192fdfc3c64877da6d77721a2564bb8df
jainshi <jainshi@gmail.com>
2013-10-10 22:15:08 +0530

    security

---
+++ learn-spring-maven/src/main/java/com/my/springlearn/board/web/MessageDeleteController.java
@@ -0,0 +1,32 @@
+package com.my.springlearn.board.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.my.springlearn.board.domain.Message;
+import com.my.springlearn.board.service.MessageBoardService;
+
+@Controller
+@RequestMapping("/messageDelete*")
+public class MessageDeleteController {
+	private MessageBoardService messageBoardService;
+
+	@Autowired
+	public void MessageDeleteController(MessageBoardService messageBoardService) {
+		this.messageBoardService = messageBoardService;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String messageDelte(
+			@RequestParam(required = true, value = "messageId") Long messageId,
+			Model model) {
+		Message message = messageBoardService.findMessageById(messageId);
+		messageBoardService.deleteMessage(message);
+		model.addAttribute("messages", messageBoardService.listMessages());
+		return "redirect:messageList";
+	}
+}
---
+++ learn-spring-maven/src/main/java/com/my/springlearn/board/web/MessageListController.java
@@ -0,0 +1,31 @@
+package com.my.springlearn.board.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.my.springlearn.board.domain.Message;
+import com.my.springlearn.board.service.MessageBoardService;
+
+@Controller
+@RequestMapping("/messageList*")
+public class MessageListController {
+	private MessageBoardService messageBoardService;
+
+	@Autowired
+	public MessageListController(MessageBoardService messageBoardService) {
+		this.messageBoardService = messageBoardService;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String generateList(Model model) {
+		List<Message> messages = java.util.Collections.emptyList();
+		messages = messageBoardService.listMessages();
+		model.addAttribute("messages", messages);
+		return "messageList";
+	}
+}
